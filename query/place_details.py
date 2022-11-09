ba9993c32a8952fe302b4b18bbb325b9d179e038
Jason Ye <ye8jason@gmail.com>
2022-11-08 22:17:55 -0500

    Maps API query scripts

---
+++ query/place_details.py
@@ -0,0 +1,99 @@
+import json, csv
+import googlemaps
+
+# Connect to Google Maps API
+API_KEY = input("Enter the API key: ")
+gmaps = googlemaps.Client(key = API_KEY)
+
+# Import business list
+business_list = open("data/search_nearby.csv", "r", encoding="utf-8-sig", newline='')
+business_data = csv.reader(business_list)
+next(business_data)
+
+# json
+json_dump = open("data/place_details_unprocessed.json", "w", encoding="utf-8-sig")
+json_export = []
+
+# csv
+data_output = open("data/place_details.csv", "w", encoding="utf-8-sig", newline='')
+csv_writer = csv.writer(data_output)
+csv_writer.writerow(["place_id", "name", "plus_code", "street_number", "route", "neighborhood", "township", "postal_code", "latitude", "longitude", "curbside_pickup", "price_level", "rating", "user_ratings_total", "mon_open", "mon_close", "tue_open", "tue_close", "wed_open", "wed_close", "thu_open", "thu_close", "fri_open", "fri_close", "sat_open", "sat_close", "sun_open", "sun_close"])
+
+# Query businesses in list
+for business in business_data:
+    # Check API call status
+    while (True):
+        response = gmaps.place(place_id=business[0])
+                    
+        if (response["status"] == "OK"):
+            break
+        else:
+            print("Request failed! Status: " + response["status"])
+            input("Press enter to retry.")
+    
+    # Process data
+    data = response["result"]
+
+    # json
+    json_export.append(response)
+
+    # csv
+    name = data.get("name")
+    place_id = data.get("place_id")
+    plus_code = data.get("plus_code").get("global_code")
+
+    address_components = data.get("address_components")
+    neighborhood = None
+    township = None
+
+    for component in address_components:
+        level = component["types"][0]
+
+        if level == "street_number":
+            street_number = component.get("long_name")
+        elif level == "route":
+            route = component.get("long_name")
+        elif level == "neighborhood":
+            neighborhood = component.get("long_name")
+        elif level == "locality":
+            pass
+        elif level == "administrative_area_level_3": # Township
+            township = component.get("long_name")
+        elif level == "administrative_area_level_2": # County
+            pass
+        elif level == "administrative_area_level_1": # State
+            pass
+        elif level == "country":
+            pass
+        elif level == "postal_code":
+            postal_code = component.get("long_name")
+
+
+    location = data.get("geometry").get("location")
+    latitude = location.get("lat")
+    longitude = location.get("lng")
+
+    curbside_pickup = data.get("curbside_pickup")
+    price_level = data.get("price_level")
+    rating = data.get("rating")
+    user_ratings_total = data.get("user_ratings_total")
+
+    details = [place_id, name, plus_code, street_number, route, neighborhood, township, postal_code, latitude, longitude, curbside_pickup, price_level, rating, user_ratings_total]
+
+    # WIP
+    opening_hours = data.get("opening_hours")
+
+    for day in opening_hours["periods"]:
+        open = day["open"]["time"]
+        close = day["close"]["time"]
+
+        details.append(open)
+        details.append(close)
+
+    csv_writer.writerow(details)
+
+json.dump(json_export, json_dump)
+
+business_list.close()
+json_dump.close()
+data_output.close()
---
+++ query/search_nearby.py
@@ -0,0 +1,94 @@
+import math, json, csv
+from PIL import Image
+import googlemaps
+
+# Config
+SPACING = 10
+
+# Connect to Google Maps API
+API_KEY = input("Enter the API key: ")
+gmaps = googlemaps.Client(key = API_KEY)
+
+# Geographical information
+SOUTH_LIMIT = 40.361370
+WEST_LIMIT = -80.095286
+NORTH_LIMIT = 40.500925
+EAST_LIMIT = -79.865750
+WEST_EAST_DIST_DEG = EAST_LIMIT - WEST_LIMIT
+WEST_EAST_DIST_KM = 19.47 # 85 km per degree longitude
+SOUTH_NORTH_DIST_DEG = NORTH_LIMIT - SOUTH_LIMIT
+SOUTH_NORTH_DIST_KM = 15.58 # 111 km per degree latitude
+
+# Calculate parameters
+RADIUS = SPACING / 2 * math.sqrt(2)
+RADIUS_M = SPACING * 1000
+X_INTERVALS = math.floor(WEST_EAST_DIST_KM / SPACING)
+X_START = 1 / X_INTERVALS / 2
+Y_INTERVALS = math.floor(SOUTH_NORTH_DIST_KM / SPACING)
+Y_START = 1 / Y_INTERVALS / 2
+
+# Import map
+map = Image.open("query/pittsburgh.png")
+map.load()
+
+# json output
+json_dump = open("data/search_nearby_unprocessed.json", "w", encoding="utf-8-sig")
+json_export = []
+
+# csv output
+data_output = open("data/search_nearby.csv", "w", encoding="utf-8-sig", newline='')
+csv_writer = csv.writer(data_output)
+csv_writer.writerow(["place_id", "name", "business_status", "formatted_address", "latitude", "longitude", "price_level", "rating", "user_ratings_total"])
+
+# Longitude
+for x in range(X_INTERVALS):
+    x_prop = X_START + x / X_INTERVALS
+    pixel_x = math.floor(x_prop * (map.width - 1))
+    longitude = WEST_LIMIT + x_prop * WEST_EAST_DIST_DEG
+
+    # Latitude
+    for y in range(Y_INTERVALS):
+        y_prop = Y_START + y / Y_INTERVALS
+        pixel_y = math.floor(y_prop * (map.height - 1))
+        latitude = NORTH_LIMIT - y_prop * SOUTH_NORTH_DIST_DEG
+
+        # Check intersection
+        if (map.getpixel((pixel_x, pixel_y)) == (0, 0, 0, 255)):
+            # Check API call status
+            while (True):
+                response = gmaps.places(location=(latitude, longitude), radius=RADIUS_M, type="restaurant")
+            
+                if (response["status"] == "OK"):
+                    break
+                else:
+                    print("Request failed! Status: " + response["status"])
+                    input("Press enter to retry.")
+
+            # Process data
+            data = response["results"]
+
+            # json
+            json_export.append(data)
+
+            # csv
+            for business in data:
+                name = business.get("name")
+                place_id = business.get("place_id")
+                business_status = business.get("business_status")
+                formatted_address = business.get("formatted_address")
+
+                location = business.get("geometry").get("location")
+                latitude = location.get("lat")
+                longitude = location.get("lng")
+
+                price_level = business.get("price_level")
+                rating = business.get("rating")
+                user_ratings_total = business.get("user_ratings_total")
+
+                csv_writer.writerow([place_id, name, business_status, formatted_address, latitude, longitude, price_level, rating, user_ratings_total])
+
+json.dump(json_export, json_dump)
+
+map.close()
+json_dump.close()
+data_output.close()
